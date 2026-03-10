c37ce5b9f4e04f8e388951671678a543b484d47e
Luiz <luizdeveloper@outlook.com>
2026-03-10 00:10:59 -0300

    🦖 Removendo tracking do 'dist'

--- dist/nodes/Pash/transport.d.ts
+++
@@ -1,2 +0,0 @@
-import type { IDataObject, IExecuteFunctions, IHttpRequestMethods } from 'n8n-workflow';
-export declare function pashApiRequest(this: IExecuteFunctions, method: IHttpRequestMethods, endpoint: string, body?: object, qs?: IDataObject): Promise<any>;
